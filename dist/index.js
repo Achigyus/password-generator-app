95a7bfd6cf4da34964cc1d5b6703d7d66470a14e
Achigyus <gyuseachi@yahoo.com>
2025-03-06 22:52:36 +0100

    worked on js

--- dist/index.js
+++ dist/index.js
@@ -18,15 +18,46 @@ function handleClick(e) {
     console.log(e);
     console.log(target.name);
     console.log(target.checked);
-    if (target.name === 'include_uppercase') {
-        console.log(passwordOptions['include_uppercase']);
+    if (target.name === 'include_uppercase' || target.name === 'include_lowercase' || target.name === 'include_numbers' || target.name === 'include_symbols') {
         let converted = Boolean(target.checked);
-        console.log('type of is', typeof (converted));
-        passwordOptions['include_uppercase'] = converted;
+        passwordOptions[`${target.name}`] = target.checked;
+        updateStrength();
     }
     else {
         console.log('no match');
     }
     console.log(passwordOptions);
 }
+function checkStrength(e) {
+    e.preventDefault();
+    let password = '';
+    let length = parseInt((slider === null || slider === void 0 ? void 0 : slider.value) || '0');
+    let charset = '';
+    if (passwordOptions.include_uppercase)
+        charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (passwordOptions.include_lowercase)
+        charset += 'abcdefghijklmnopqrstuvwxyz';
+    if (passwordOptions.include_numbers)
+        charset += '0123456789';
+    if (passwordOptions.include_symbols)
+        charset += '!@#$%^&*()_+';
+    for (let i = 0; i < length; i++) {
+        password += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+    console.log('Generated password:', password);
+    return password;
+}
+function updateStrength() {
+    let strength = 0;
+    if (passwordOptions.include_uppercase)
+        strength++;
+    if (passwordOptions.include_lowercase)
+        strength++;
+    if (passwordOptions.include_numbers)
+        strength++;
+    if (passwordOptions.include_symbols)
+        strength++;
+    console.log('Updated strength:', strength);
+}
 form === null || form === void 0 ? void 0 : form.addEventListener('input', handleClick);
+form === null || form === void 0 ? void 0 : form.addEventListener('submit', checkStrength);
